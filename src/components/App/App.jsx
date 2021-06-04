89947ec11311deefa0e64ad55c8a50a3f33f528d
Ryan Maas <rdmjobs@live.com>
2021-06-04 11:48:27 -0500

    Cleaned up code, added comments.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,67 +4,69 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useEffect } from 'react';
 import Header from '../Header/Header.jsx'
 import axios from 'axios';
+import AddItemForm from '../AddItemForm/AddItemForm';
 //#endregion ⬆⬆ All document setup above. 
 
 
 function App() {
 
-const addItem = (newItem) => {
-    console.log(newItem);
+
+
+  //#region ⬇⬇ All state variables & app load below:
+  // ⬇ State variables:
+  const [item, setItem] = useState([]);
+  // ⬇ On load, get items:
+  useEffect(() => {
+    getItems();
+  }, []);
+  //#endregion ⬆⬆ All state variables & app load above. 
+
+
+  //#region ⬇⬇ All CRUD routes below:
+  // GET: 
+  const getItems = () => {
+    console.log('In getItems');
+    // ⬇ Axios GET to pull from DB:
+    axios.get('/list')
+      .then(response => {
+        console.log('logging response data in get', response.data);
+        setItem(response.data);
+      }) // End .then
+      .catch(error => {
+        alert('error getting items');
+        console.log('error in get', error);
+      }); // End .catch
+  } // End getItems
+
+  // POST: 
+  const addItem = (newItem) => {
+    console.log('In newItem, newItem is:', newItem);
     // AXIOS POST
-    axios.post('/items',  newItem)
-    .then(response => {
+    axios.post('/list', newItem)
+      .then(response => {
         console.log(`POSTED ${newItem}`);
         // call getter function
-    }).catch(err => {
+      }) // End .then
+      .catch(err => {
         console.log('failed to post', err);
-    })
+      }); // End catch
+  }; // End addItem
 
-}
-
-    //#region ⬇⬇ All state variables & app load below:
-    // ⬇ State variables:
-    const [item, setItem] = useState([]);
-    // ⬇ On load, get items:
-    useEffect( () => {
-      getItems();
-      }, []);
-    //#endregion ⬆⬆ All state variables & app load above. 
+  //#endregion ⬆⬆ All CRUD routes above. 
 
 
-    //#region ⬇⬇ All CRUD routes below:
-    const getItems = () => {
-        console.log('In getItems');
-        // ⬇ Axios GET to pull from DB:
-        axios.get('/list')
-        .then(response => {
-            console.log('logging response data in get', response.data);
-            setItem(response.data);
-        }) // End .then
-        .catch(error => {
-            alert('error getting items');
-            console.log('error in get', error);
-        }); // End .catch
-    } // End getItems
+  //#region ⬇⬇ All rendering below:
+  return (
+    <div className="App">
+      <Header />
+      <AddItemForm />
+      <main>
 
-
-
-
-
-    //#endregion ⬆⬆ All CRUD routes above. 
-
-
-    //#region ⬇⬇ All rendering below:
-    return (
-        <div className="App">
-            <Header />
-            <main>
-                
-                <p>Under Construction...</p>
-            </main>
-        </div>
-    ); // End return
-    //#endregion ⬆⬆ All rendering above. 
+        <p>Under Construction...</p>
+      </main>
+    </div>
+  ); // End return
+  //#endregion ⬆⬆ All rendering above. 
 } // End App
 
 
