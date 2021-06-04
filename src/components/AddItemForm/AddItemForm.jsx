5befa2e2da9a6dfb9f43f8f3827285304788cac2
Terry O'Keefe <terry.okeefe87@gmail.com>
2021-06-04 13:46:26 -0500

    working on return in function

--- src/components/AddItemForm/AddItemForm.jsx
+++ src/components/AddItemForm/AddItemForm.jsx
@@ -8,13 +8,29 @@ function AddItemForm ({addItem}) {
    // function to handle submit
    const handleSubmit = (event) => {
       event.preventDefault();
-      AddItem(newItem);
+      addItem(newItem);
    }
  return(
 <form onSubmit={handleSubmit}>
-   <label htmlFor="">Item</label> <input type="text" />
-   <label htmlFor="">Quantity</label> <input type="text" />
-   <label htmlFor="">Unit</label> <input type="text" />
+   <label htmlFor="">Item</label> 
+      <input onChange={(event) => setNewItem({name: event.target.value})} 
+         value={newItem.name} 
+         placeholder="Item"
+         type="text"
+      />
+   <label htmlFor="">Quantity</label> 
+   <input onChange={(event) => setNewItem({quantity: event.target.value})} 
+         value={newItem.quantity} 
+         placeholder="Quantity"
+         type="text"
+      />
+  
+   <label htmlFor="">Unit</label> 
+   <input onChange={(event) => setNewItem({unit: event.target.value})} 
+         value={newItem.unit} 
+         placeholder="Unit"
+         type="text"
+      />
    <input type="submit" value="Save" />
 </form>
  );   
